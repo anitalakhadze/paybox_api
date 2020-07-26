310ace94e0e1d6744fa5e6be707c924ef77ca496
atala15 <atala15@freeuni.edu.ge>
2020-07-26 10:35:24 +0400

    modified the implementation

--- src/main/java/com/anita/paybox_api/product_categories/MobilePayment.java
+++ src/main/java/com/anita/paybox_api/product_categories/MobilePayment.java
@@ -1,5 +1,27 @@
 package com.anita.paybox_api.product_categories;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+@Entity
 public class MobilePayment {
     
+    private @Id @GeneratedValue Long id;
+    private String phoneNumber; 
+    private Double amount;
+    private Double commission;
+    private String serviceType;
+
+    public MobilePayment() {}
+
+    public MobilePayment(String phoneNumber, 
+        Double amount, Double commission, 
+        String serviceType){
+
+        this.phoneNumber = phoneNumber;
+        this.amount = amount;
+        this.commission = commission;
+        this.serviceType = serviceType;
+    }
 }
